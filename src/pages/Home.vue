d34569f056de8cf3dc7d85a933c0914846a3b0cf
Graham Pegg (MacBook) <graham@citrusmedia.com>
2016-11-29 23:17:52 -0500

    Message to rob

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,44 @@
 <template>
-  <span>huh</span>
+  <div class="page">
+    <div class="center">
+      <h1>Hey Rob!</h1>
+      <h3>Graham Pegg, Humber Lakeshore Campus, N01180081</h3>
+      <p>
+        Been trying to come up with an idea of what to do with this domain...
+      </p>
+    </div>
+  </div>
 </template>
+
+<style>
+* {
+  font-family: Arial, sans-serif;
+  color: #232323;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+body {
+  background: #efefef;
+  height: 100vh;
+  margin: 0;
+}
+
+.center {
+  flex: 1 1 100%;
+  padding: 0 30px;
+}
+
+.page {
+  width: 100vw;
+  max-width: 970px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  font-size: 3rem;
+}
+</style>
