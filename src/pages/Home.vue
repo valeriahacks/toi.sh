eb8d2bc6879caf0c9fe7f737003e734064a39553
Graham Pegg (MacBook) <graham@citrusmedia.com>
2016-12-27 23:59:33 -0500

    Updated scss utils

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="page">
+    <div class="greeting">
+      <h2><odometer>Hi!</odometer></h2>
+    </div>
     <math></math>
   </div>
 </template>
 
 <script>
   import Math from '../components/Math.vue'
+  import Odometer from '../components/Odometer.vue'
 
   export default {
-    components: { Math }
+    components: { Math, Odometer }
   }
 </script>
 
 <style lang="scss">
   @import '../sass/sass-only/app';
 
-  .page {
-    color: $warmgrey;
+  .greeting {
+    h2 {
+      font-family: $reg;
+      font-weight: bold;
+      font-size: 5rem;
+    }
   }
 </style>
