43122b1f127674712c6e22d09e0b5e7c37e69564
Graham Pegg (MacBook) <graham@citrusmedia.com>
2016-12-30 03:37:20 -0500

    IDK, it's something

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <header :class="($route.name === 'home' ? 'home' : 'interior')">
+    <header :class="($route.name === 'home' ? 'home' : 'interior')"
+            :style="headerHeight">
       <div class="logo">
         <router-link to="/" v-html="require('./assets/svg/logo.svg')">
           <!-- SVG file injected here by webpack -->
@@ -21,6 +22,31 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        scroll: window.pageYOffset || document.documentElement.scrollTop
+      }
+    },
+    computed: {
+      headerHeight () {
+        let scaler = 1 - (this.scroll / 100)
+        let scaled = ((scaler > 1 ? 1 : scaler) * 50) + 70
+        return 'height:' + scaled + 'px'
+      }
+    },
+    methods: {
+      updateScroll () {
+        this.scroll = window.pageYOffset || document.documentElement.scrollTop
+      }
+    },
+    mounted () {
+      window.addEventListener('scroll', this.updateScroll.bind(this))
+    }
+  }
+</script>
 
 <style lang="scss">
   @import './sass/main.scss';
