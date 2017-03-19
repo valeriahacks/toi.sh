0bfba443892aa3e267b1d7a008159e0c3ed04721
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-03-19 17:16:03 -0400

    Almost got this word scroller thing working how I want it to

--- src/components/Scroller.vue
+++ src/components/Scroller.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="scroller">
-    <div class="word" v-for="word in words">
+    <div
+      v-for="word in words"
+      :class="'word ' + getWordClass(word)"
+      @click="(getWordAction(word))">
       {{ word }}
     </div>
   </div>
@@ -21,16 +24,44 @@
     },
     computed: {
       max () {
-        return this.word.length
+        return this.words.length
       },
       currentWord () {
         return this.words[this.i]
+      },
+      nextInWord () {
+        return this.words[(this.i + 2) % this.max]
       },
       nextWord () {
         return this.words[(this.i + 1) % this.max]
       },
       prevWord () {
-        return this.words[((this.i % this.max) + this.max) % this.max]
+        return this.words[((this.i - 1 % this.max) + this.max) % this.max]
+      },
+      prevOutWord () {
+        return this.words[((this.i - 2 % this.max) + this.max) % this.max]
+      }
+    },
+    methods: {
+      getWordClass (word) {
+        if (word === this.currentWord) return 'centre'
+        if (word === this.nextWord) return 'top'
+        if (word === this.prevWord) return 'bottom'
+        if (word === this.nextInWord) return 'top-in'
+        if (word === this.prevOutWord) return 'bottom-out'
+        return ''
+      },
+      getWordAction (word) {
+        if (word === this.currentWord) return this.next
+        if (word === this.nextWord) return this.next
+        if (word === this.prevWord) return this.prev
+        return () => {}
+      },
+      next () {
+        this.i = (this.i + 1) % this.max
+      },
+      prev () {
+        this.i = ((this.i - 1 % this.max) + this.max) % this.max
       }
     }
   }
@@ -40,24 +71,41 @@
   .scroller {
     display: inline-block;
     position: relative;
+    height: 30px;
 
     .word {
       position: absolute;
       top: 0;
       left: 0;
-      transform: perspective(500px) translateX(-125%) rotateX(90deg);
-      transition: 0.5s transform ease;
+      user-select: none;
+      cursor: pointer;
+      transform: perspective(500px) translateY(-125%) rotateX(90deg);
+      opacity: 0;
+      transition: 0.5s transform ease, 1s opacity ease;
+
+      &.top-in {
+        transform: perspective(900px) translateY(-130%) rotateX(90deg);
+        opacity: 1;
+      }
 
       &.top {
-        transform: perspective(500px) translateX(-100%) rotateX(45deg);
+        transform: perspective(900px) translateY(-100%) rotateX(45deg);
+        opacity: 1;
       }
 
       &.centre {
-        transform: perspective(500px) translateX(0) rotateX(0deg);
+        transform: perspective(900px) translateY(0) rotateX(0deg);
+        opacity: 1;
       }
 
       &.bottom {
-        transform: perspective(500px) translateX(100%) rotateX(-45deg);
+        transform: perspective(900px) translateY(100%) rotateX(-45deg);
+        opacity: 1;
+      }
+
+      &.bottom-out {
+        transform: perspective(900px) translateY(130%) rotateX(-90deg);
+        opacity: 1;
       }
     }
   }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -28,7 +28,16 @@
         <scroller :words="[
           'functional programming',
           'red pandas',
-          'colour theory'
+          'colour theory',
+          '2functional programming',
+          '2red pandas',
+          '2colour theory',
+          '3functional programming',
+          '3red pandas',
+          '3colour theory',
+          '4functional programming',
+          '4red pandas',
+          '4colour theory',
         ]" />
       </p>
     </main>
