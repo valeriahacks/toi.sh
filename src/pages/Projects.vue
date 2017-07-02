e85f2626ac44f16d58aca71a3566393d1d5ab363
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-07-02 17:19:53 -0400

    Working on projects page a little

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="page">
-    <h3>
-      <odometer delay="0" speed="65">Projects</odometer>
-    </h3>
-    <div v-html="require('../assets/svg/unfuck.svg')"></div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <h3>My Projects</h3>
+    <div class="projects">
+
+      <div class="project">
+        <div class="icon" v-html="require('../assets/svg/unfuck.svg')"></div>
+        <h4>Chromatism</h4>
+        <p>Lorem ipsum dolar sit amet</p>
+      </div>
+
+      <div class="project">
+        <div class="icon" v-html="require('../assets/svg/unfuck.svg')"></div>
+        <h4>Unfuck</h4>
+        <p>Lorem ipsum dolar sit amet</p>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -15,10 +27,39 @@
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   @import '../sass/sass-only/app';
 
-  svg {
-    width: 200px;
+  .page { display: block; }
+
+  .projects {
+    display: flex;
+
+    .project {
+      flex: 1 1 200px;
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      padding: 30px;
+      position: relative;
+      color: #888;
+
+      h4 {
+        flex: 0 1 150px;
+        padding: 15px 0;
+        margin: 0;
+        font-size: 2rem;
+        text-align: center;
+      }
+
+      p {
+        flex: 1 1 200px;
+      }
+
+      .icon {
+        width: 100%;
+        max-width: 90px;
+      }
+    }
   }
 </style>
