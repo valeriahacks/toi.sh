b595eb12ef749dcac970e2fa503a2f11dca02a8e
Graham Pegg (MacBook) <graham@citrusmedia.com>
2017-08-07 10:09:50 -0400

    Cool looking equation

--- src/js/equations.js
+++ src/js/equations.js
@@ -1,14 +1,15 @@
 /* eslint-disable */
 
 export default [
-/*0*/	(x, y, n) => (~n ^ y | ~x) + Math.sqrt(x + n) * 255,
-/*1*/	(x, y, n) => Math.floor(Math.log(x | y & n) * n ^ 20 | Math.sqrt(x * n) * y & ~n),
-/*2*/	(j, i, x) => Math.floor(Math.sqrt(Math.pow(x ^ i ^ j, 1 / i) / j) * (x ^ Math.sqrt(i)) * 255),
-/*3*/	(j, i, x) => Math.floor(Math.sin((Math.sin(x - j / 2) > 0 ? j : i) + x) * 255),
-/*4*/	(j, i, x) => Math.floor(Math.sin(x - j % (i ^ x)) * 255),
-/*5*/	(j, i, x) => Math.floor(Math.sin(Math.max(j, i, x) - Math.min(j, i, x)) * 255),
-/*6*/	(j, i, x) => Math.floor(Math.sin(Math.sin(x) + Math.sin(j) + Math.sin(i)) * 255),
-/*7*/	(j, i, x) => Math.floor(Math.sin(j + Math.sin(i) * x) * 255),
-/*8*/	(x, y, n) => n * (y + x),
-/*9*/	(x, y, n) => n * (x / y) | x ^ y,
+  /*0*/	function(t,e,n){return(~n^e|~t)+255*Math.sqrt(t+n)},
+  /*1*/	function(t,e,n){return Math.floor(Math.log(t|e&n)*n^20|Math.sqrt(t*n)*e&~n)},
+  /*2*/	function(t,e,n){return Math.floor(Math.sqrt(Math.pow(n^e^t,1/e)/t)*(n^Math.sqrt(e))*255)},
+  /*3*/	function(t,e,n){return Math.floor(255*Math.sin((Math.sin(n-t/2)>0?t:e)+n))},
+  /*4*/	function(t,e,n){return Math.floor(255*Math.sin(n-t%(e^n)))},
+  /*5*/	function(t,e,n){return Math.floor(255*Math.sin(Math.max(t,e,n)-Math.min(t,e,n)))},
+  /*6*/	function(t,e,n){return Math.floor(255*Math.sin(Math.sin(n)+Math.sin(t)+Math.sin(e)))},
+  /*7*/	function(t,e,n){return Math.floor(255*Math.sin(t+Math.sin(e)*n))},
+  /*8*/	function(t,e,n){return n*(e+t)},
+  /*9*/	function(t,e,n){return n*(t/e)|t^e},
+  /*10*/  function(x, y, n){return Math.sin(n/50)*x*y}
 ]
